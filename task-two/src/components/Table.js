1adace34684d3ffe562888885c3029e0acc40b8b
david-osas <azamegbeosarumense@gmail.com>
2020-12-30 15:45:31 +0100

    Add loading

--- task-two/src/components/Table.js
+++ task-two/src/components/Table.js
@@ -1,20 +1,29 @@
 import {useEffect, useState} from 'react';
 import Row from './Row';
+import loadingImg from '../loading.gif';
 
 function Table(){
   let [data, setData] = useState([]);
+  let [load, setLoad] = useState(true);
   let titles = ['State', 'Confirmed cases','Cases on admission', 'Discharged', 'Death'];
 
   async function getData(){
     let res = await fetch('https://covidnigeria.herokuapp.com/api');
     let resJson = await res.json();
     setData(resJson.data.states);
+    setLoad(false);
   }
 
   useEffect(() => {
     getData();
 
   },[])
+
+  if(load){
+    return(
+      <img src={loadingImg} alt='loading' className='loading' />
+    )
+  }
 
   return(
     <div className='app-container'>
